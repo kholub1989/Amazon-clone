de0580c95576159a9f5c076a54b1ef4c7126b43d
Krisztian Holub <kholub1989@gmail.com>
2020-09-22 21:07:51 -0400

    building Checkout page

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,22 @@
 export const initialState = {
-  basket: [],
+  basket: [
+    {
+      id: "3423414",
+      title: "Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future",
+      price: 10.99,
+      rating: 4,
+      image:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1466251246i/30643314._UY500_SS500_.jpg",
+    },
+    {
+      id: "3423414",
+      title: "Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future",
+      price: 10.99,
+      rating: 4,
+      image:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1466251246i/30643314._UY500_SS500_.jpg",
+    },
+  ],
   user: null,
 };
 
@@ -11,7 +28,23 @@ const reducer = (state, action) => {
       return { ...state, basket: [...state.basket, action.item] };
     case "REMOVE_FROM_BASKET":
       // Logic for Removing item from basket
-      return { state };
+
+      // I clone the basket
+      let newBasket = [...state.basket];
+
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+
+      if (index >= 0) {
+        // item exists in basket, remove it...
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Cant remove product (id: ${action.id} as its not in basket)`
+        );
+      }
+      return { ...state, basket: newBasket };
     default:
       return state;
   }
